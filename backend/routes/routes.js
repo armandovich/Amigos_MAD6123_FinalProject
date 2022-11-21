dd254d20e3b8959738246026c4128b3bd0af4f9d
Daniel Miolan <miolan96@gmail.com>
2022-11-20 21:20:46 -0500

    BackEnd - Get all project from DB.

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const ProjectModel = require('../models/project');
 
 const router = express.Router()
 
 module.exports = router;
 
 router.get('/projects', async (req, res) => {
-    console.log('GET ALL PROJECT.');
+    try{
+        const data = await ProjectModel.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
