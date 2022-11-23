93aae6fbefbe935abf2bd7884b76d17c2bd109c8
Daniel Miolan <miolan96@gmail.com>
2022-11-23 10:08:36 -0500

    FrontEnd - Create project CRUD layout and style.

--- frontend/admin/ProjectManager.js
+++ frontend/admin/ProjectManager.js
@@ -1,21 +1,128 @@
 import { useEffect, useState } from "react";
+import { Ionicons } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, Text, Pressable, View } from 'react-native';
+import { StyleSheet, Text, Pressable, TextInput, ScrollView, View } from 'react-native';
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
 
 export default function ProjectManager({navigation, route}) {
-    const goBack = () => {
-        navigation.goBack()
+  // Hold params data from route
+  const [ediMode, setEditMode] = useState(route.params.isEdit);
+  const [project, setProject] = useState(route.params.project);
+  // Project Model Attributes
+  const [name, setName] = useState();
+  const [desc, setDesc] = useState();
+  const [starDate, setStartDate] = useState('YYYY-MM-DD');
+  const [endDate, setEndDate] = useState('YYYY-MM-DD');
+  const [status, setStatus] = useState('Ongoing');
+  const [tasks, setTasks] = useState(0);
+  const [tasksComplete, setTasksComplete] = useState(0);
+  const [cost, setCost] = useState(0);
+  // Hold which date should be update with the date picker
+  const [pickStartDate, setPickStartDate] = useState(false);
+
+  const [date, setDate] = useState(new Date());
+
+  const formatDate = (value) => {
+    return new Date(value).toISOString().slice(0, 10);
+  }
+
+  const onChange = (event, selectedDate) => {
+    if(pickStartDate) {
+      setStartDate(formatDate(selectedDate));
+    } else {
+      setEndDate(formatDate(selectedDate));
     }
+  };
+  
+  const goBack = () => {
+      navigation.goBack()
+  }
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.headline}>PROJECT MANAGER</Text>
+  const showMode = (currentMode) => {
+    DateTimePickerAndroid.open({
+      value: date,
+      onChange,
+      mode: currentMode,
+      is24Hour: true,
+    });
+  };
 
-            <Pressable onPress={goBack} style={styles.logout}>
-                <Text style={[styles.btnTxt]}>RETURN</Text>
+  const showDatepicker = (isStartDate) => {
+    setPickStartDate(isStartDate);
+    showMode('date');
+  };
+    
+  useEffect(() => {
+    if (project) {
+      setName(project.name);
+      setDesc(project.description);
+      setStatus(project.status);
+      setStartDate(formatDate(project.start_date));
+      setEndDate(formatDate(project.end_date));
+      setTasks(project.task_number);
+      setTasksComplete(project.task_complete);
+      setCost(project.total_cost);
+    }
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Pressable onPress={goBack} style={styles.head}>
+        <Ionicons name="arrow-back" size={24} color="#fff" />
+        {ediMode ? 
+          <Text style={styles.headline}>PROJECT EDITING</Text>
+        :
+        <Text style={styles.headline}>PROJECT CREATION</Text>
+        }
+      </Pressable>
+
+      <ScrollView style={styles.scroll}>
+        <TextInput style={styles.inputs} value={name} placeholder='Name'/>
+
+        <TextInput style={styles.inputs} value={desc} placeholder='Description'/>
+
+        <Text style={styles.whiteTxt}>Start Date: </Text>
+        <Pressable onPress={() => showDatepicker(true)} style={styles.datePicker}>
+          <Text style={styles.datePickerTxt}>{starDate}</Text>
+        </Pressable>
+
+        <Text style={styles.whiteTxt}>End Date: </Text>
+        <Pressable onPress={() => showDatepicker(false)} style={styles.datePicker}>
+          <Text style={styles.datePickerTxt}>{endDate}</Text>
+        </Pressable>
+
+
+        {ediMode ? 
+          <>
+            <View style={styles.infoRow}>
+              <Text style={styles.whiteTxt}>Task Number: </Text>
+              <Text style={styles.whiteTxt}>{tasks}</Text>
+            </View>
+
+            <View style={styles.infoRow}>
+              <Text style={styles.whiteTxt}>Total Cost: </Text>
+              <Text style={styles.whiteTxt}>{cost}</Text>
+            </View>
+
+            <Pressable style={[styles.btn, styles.btnGreen]}>
+                <Text style={styles.btnTxt}>Update</Text>
             </Pressable>
-        </SafeAreaView>
-    );
+
+            <Pressable style={[styles.btn, styles.btnRed]}  onPress={() => goToScreen('Login')} >
+                <Text style={[styles.btnTxt]}>Delete</Text>
+            </Pressable>
+          </>
+        :
+          <Pressable style={[styles.btn, styles.btnGreen]}>
+            <Text style={styles.btnTxt}>Create</Text>
+          </Pressable>
+        }
+
+        
+
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -24,29 +131,72 @@ const styles = StyleSheet.create({
       backgroundColor: '#000',
       alignItems: 'flex-start',
       justifyContent: 'flex-start',
+    },
+    scroll: {
+      width: '100%',
+      paddingHorizontal: 15
+    },
+    head: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
       paddingHorizontal: 15,
     },
     headline: {
       fontWeight: 'bold',
       fontSize: 25,
+      paddingLeft: 15,
       color: '#fff',
       paddingVertical: 15,
     },
-    logout: {
+    inputs: {
+      backgroundColor: '#fff',
+      width: '100%',
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      borderRadius: 8,
+      marginBottom: 15
+    },
+    datePicker: {
+      backgroundColor: '#fff',
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      borderRadius: 8,
+      marginBottom: 15
+    },
+    datePickerTxt: {
+      color: '#959595',
+      paddingVertical: 5,
+    },
+    btn: {
       width: '100%',
       paddingHorizontal: 15,
       marginBottom: 15,
       borderRadius: 8,
       paddingVertical: 15,
-      paddingHorizontal: 15,
-      backgroundColor: '#BF1700',
-      maxWidth: 200,
-      marginTop: 15
-    },
-    btnTxt: {
+      paddingHorizontal: 15
+  },
+  btnTxt: {
       color: '#fff',
       textAlign: 'center',
       fontWeight: 'bold'
+    },
+    btnGreen: {
+        backgroundColor: '#84B026'
+    },
+    btnRed: {
+      backgroundColor: '#BF1700'
+    },
+    btnBorder: {
+        borderWidth: 2,
+        borderColor: '#84B026'
+    },
+    greenTxt: {
+        color: '#84B026',
+    },
+    infoRow: {
+      flexDirection: 'row',
+      marginBottom: 15
     },
     whiteTxt: {
       color: '#fff',
--- frontend/screens/Projects.js
+++ frontend/screens/Projects.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
@@ -9,6 +10,7 @@ const dummyData = [
         description: "New description...",
         status: "Ongoing",
         task_number: 5,
+        task_complete: 2,
         total_cost: 0,
         start_date: "2022-01-20T00:00:00.000Z",
         end_date: "2022-02-01T00:00:00.000Z",
@@ -19,6 +21,7 @@ const dummyData = [
         description: "New description...",
         status: "Ongoing",
         task_number: 0,
+        task_complete: 2,
         total_cost: 0,
         start_date: "2022-01-20T00:00:00.000Z",
         end_date: "2022-02-01T00:00:00.000Z",
@@ -28,16 +31,16 @@ const dummyData = [
 export default function Projects({navigation}) {
     const [projectList, setProjectList] = useState(dummyData);
 
-    const goToRegister = () => {
-        navigation.navigate({ name: 'Profile' });
-    }
-
     const openProject = (index) => {
-        navigation.navigate('Task', { edit: false })
+        navigation.navigate('Task', { index: index })
     }
 
     const editProject = (index) => {
-        navigation.navigate('ProjectManager', { edit: true })
+        navigation.navigate('ProjectManager', { project: projectList[index], isEdit: true })
+    }
+
+    const createProject = () => {
+        navigation.navigate('ProjectManager', { project: null, isEdit: false })
     }
 
     const formatDate = (value) => {
@@ -47,31 +50,41 @@ export default function Projects({navigation}) {
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.headline}>PROJECTS</Text>
-             <FlatList style={styles.scroll} data={projectList} renderItem={({ item, index }) => 
+             <FlatList style={styles.scroll} data={projectList} 
+             ListHeaderComponent={
+                <Pressable onPress={createProject} style={styles.createBtn}>
+                    <Text style={styles.createTxt}>Add Project</Text>
+                    <Ionicons name="add-circle-outline" size={24} color="#84B026" />
+                </Pressable>
+             }
+             renderItem={({ item, index }) => 
                 <View style={styles.card}>
-                    <View style={styles.padding}>
-                        <Text style={[styles.cardName, styles.whiteTxt]}>{item.name}</Text>
+                    <View style={styles.cardPadding}>
+                        <Text style={[styles.cardName, styles.greenTxt]}>{item.name}</Text>
 
                         <View style={styles.cardTask}>
-                            <Text style={[styles.boldTxt, styles.whiteTxt]}>{item.task_number} </Text> 
-                            <Text style={styles.whiteTxt}>Tasks</Text>
+                            <Text style={[styles.boldTxt, styles.greenTxt]}>{item.task_number} </Text> 
+                            { item.task_number < 2 
+                            ? <Text style={styles.whiteTxt}>Task</Text>
+                            : <Text style={styles.whiteTxt}>Tasks</Text>
+                            }
                         </View>
                         
                         <View style={styles.cardTask}>
                             <Text style={styles.whiteTxt}>Status: </Text>
-                            <Text style={[styles.boldTxt, styles.whiteTxt]}>{item.status} </Text> 
+                            <Text style={[styles.boldTxt, styles.greenTxt]}>{item.status} </Text> 
                         </View>
 
                         <View style={styles.cardTask}>
                             <Text style={styles.whiteTxt}>Due Date: </Text>
-                            <Text style={[styles.boldTxt, styles.whiteTxt]}>{formatDate(item.end_date)} </Text> 
+                            <Text style={[styles.boldTxt, styles.greenTxt]}>{formatDate(item.end_date)} </Text> 
                         </View>
                     </View>
 
                     <Pressable onPress={() => openProject(index) } style={styles.exit}></Pressable>
 
                     <Pressable onPress={() => editProject(index) } style={styles.edit}>
-                        <FontAwesome name="edit" size={24} color="#fff" />
+                        <FontAwesome name="edit" size={24} color="#84B026" />
                     </Pressable>
                 </View>
             }/>
@@ -87,9 +100,24 @@ const styles = StyleSheet.create({
         alignItems: 'flex-start',
         justifyContent: 'flex-start',
     },
+    createBtn: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 15,
+        marginBottom: 15,
+        borderColor: '#84B026',
+        borderWidth: 2,
+        borderRadius: 5
+    },
+    createTxt: {
+        color: '#84B026',
+        paddingRight: 10
+    },
     edit: {
-        width: 40,
-        height: 40,
+        width: 50,
+        height: 50,
         position: 'absolute',
         right: 0,
         top: 0,
@@ -121,13 +149,15 @@ const styles = StyleSheet.create({
         marginBottom: 15,
         position: 'relative'
     },
-    padding: {
+    cardPadding: {
         paddingHorizontal: 10,
-        paddingVertical: 15
+        paddingTop: 15,
+        paddingBottom: 24
     },
     cardName: {
         fontSize: 20,
-        marginBottom: 5
+        marginBottom: 5,
+        fontWeight: 'bold'
     },
     cardTask: {
         flexDirection: 'row',
@@ -138,6 +168,9 @@ const styles = StyleSheet.create({
     },
     whiteTxt: {
         color: '#fff'
+    },
+    greenTxt: {
+        color: '#84B026'
     }
   });
   
